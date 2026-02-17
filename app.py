027c5aff5d3d3192cc27994b89368ed001016c72
sumannaskar036 <suman.naskar036@gmail.com>
2026-02-17 13:53:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -752,7 +752,7 @@ if st.session_state.current_data:
             with st.spinner("Consulting stars..."):
                 try:
                     # 1. Authenticate with the NEW key and FORCE the REST transport
-                    genai.configure(api_key=st.secrets["GEMINI_API_KEY_NEW"], transport='rest')
+                    genai.configure(api_key=st.secrets["AIzaSyCGKnqqGkSoC44Mr1VK_L8N5wfjFS4hcGM"], transport='rest')
                     
                     # 2. Use the 'models/' prefix to force the Stable API
                     model = genai.GenerativeModel('models/gemini-1.5-flash')
@@ -768,6 +768,6 @@ if st.session_state.current_data:
                     # Show the REAL error
                     st.error(f"Technical Error: {e}")
         
-        else:
-            st.title("🔮 TaraVaani")
-            st.info("👈 Enter details to generate chart.")
+else:
+    st.title("🔮 TaraVaani")
+    st.info("👈 Enter details to generate chart.")
